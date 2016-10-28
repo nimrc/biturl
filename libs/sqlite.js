c3b77212c2849876d28661fbc6e7c35c4744ea50
fyibmsd <fyibmsd@gmail.com>
2016-10-28 23:07:06 +0800

    fix package

--- libs/sqlite.js
+++ libs/sqlite.js
@@ -6,20 +6,18 @@ const config   = require( "../config/db" );
 
 class Sqlite {
     constructor() {
-        this.handler     = new sqlite3.Database( `${__dirname}/../${config.path}` );
-        this.handler.run = bluebird.promisify( this.handler.run );
+        this.handler = new sqlite3.Database( `${__dirname}/../${config.path}` );
 
         this.init();
     }
 
     init() {
         this.handler.run( "CREATE TABLE IF NOT EXISTS bitm (id integer primary key AutoIncrement, url text, hash varchar(20), timestamp integer)", () => {
-            this.handler.run( "CREATE UNIQUE INDEX IF NOT EXISTS hash_index on bitm (hash)", () => {
-                this.save_scheme = this.handler.prepare( "INSERT INTO bitm (url, hash, timestamp) VALUES (?, ?, ?)" );
-                this.find_scheme = this.handler.prepare( "SELECT * FROM bitm where hash = ?" );
+            this.handler.run( "CREATE UNIQUE INDEX IF NOT EXISTS hash_index on bitm (hash)" );
+            this.save_scheme = this.handler.prepare( "INSERT INTO bitm (url, hash, timestamp) VALUES (?, ?, ?)" );
+            this.find_scheme = this.handler.prepare( "SELECT * FROM bitm where hash = ?" );
 
-                this.find_scheme.get = bluebird.promisify( this.find_scheme.get );
-            } );
+            this.find_scheme.get = bluebird.promisify( this.find_scheme.get );
         } );
 
     }
